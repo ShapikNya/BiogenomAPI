1714ad706e921b82df0cf64d0858e578fc184da0
ShapikNya <Shapik_psn@mail.ru>
2026-02-15 20:50:12 +0300

    feat(persistence): add automatic database creation and migration on startup

--- Biogenom.Backend/Biogenom.WebApi/Program.cs
+++ Biogenom.Backend/Biogenom.WebApi/Program.cs
@@ -1,6 +1,17 @@
+using Biogenom.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPersistence(builder.Configuration);
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
 app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
