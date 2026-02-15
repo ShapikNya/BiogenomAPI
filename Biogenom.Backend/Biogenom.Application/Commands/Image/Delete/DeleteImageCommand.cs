ea07a2d716ee10c61e4f75072317e1a0b65b75c0
ShapikNya <Shapik_psn@mail.ru>
2026-02-15 20:19:27 +0300

    feat(application): Implement image upload and delete commands

---
+++ Biogenom.Backend/Biogenom.Application/Commands/Image/Delete/DeleteImageCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biogenom.Application.Commands.Image.Delete
+{
+    public class DeleteImageCommand : IRequest<Unit>
+    {
+        public Guid ImageId { get; set; }
+    }
+}
---
+++ Biogenom.Backend/Biogenom.Application/Commands/Image/Delete/DeleteImageCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Biogenom.Application;
+using Biogenom.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Biogenom.Application.Commands.Image.Delete
+{
+    public class DeleteImageCommandHandler : IRequestHandler<DeleteImageCommand, Unit>
+    {
+        private readonly IBiogenomDbContext _dbContext;
+
+        public DeleteImageCommandHandler(IBiogenomDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteImageCommand request, CancellationToken cancellationToken)
+        {
+            var image = await _dbContext.Images
+                .Include(i => i.DetectedObjects)
+                .FirstOrDefaultAsync(i => i.Id == request.ImageId, cancellationToken);
+
+            if (image == null)
+                throw new KeyNotFoundException("Изображение не найдено.");
+
+            // Удаляем файл с диска
+            var fullPath = Path.Combine(AppContext.BaseDirectory, image.FilePath);
+            if (File.Exists(fullPath))
+                File.Delete(fullPath);
+
+            // Удаляем запись из БД (включая связанные DetectedObjects)
+            _dbContext.Images.Remove(image);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
